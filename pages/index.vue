593485be69b7f3e180b225090821af2f11d11ab5
Apexo01 <glebpozbnakov62@gmail.com>
2023-11-10 21:20:14 +0300

    Feat: инициализация проекта

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav>
+    <p class="logo">Tercode</p>
+    <div>
+      <a href="/projetcs">Проекты</a>
+      <a href="">Обо мне</a>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
